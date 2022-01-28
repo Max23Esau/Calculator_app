f366fe621be41be042a2b4990065eb86f29f57fb
Max23Esau <esa-max23@live.com>
2022-01-27 21:20:26 -0600

    funcion del agregada

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 let screen = document.getElementById('screen');
-let result = document.getElementById('result');
+// let result = document.getElementById('result');
 const buttons = document.querySelectorAll("#buttons button");
 
+
+
 for (const button of buttons) {
-  button.addEventListener('click', function(e) {
-      e.preventDefault();
-      
-      console.log(e.target.dataset);
-      
-      if (e.target.dataset.key == 'equal') {
-          result.textContent = eval(screen.textContent);
-          if (result.textContent.length > 8) {
-              result.textContent = eval(screen.textContent).toFixed(8);
-          }
-      } else if (e.target.dataset.key == 'clear') {
-          screen.textContent = '';
-          result.textContent = '';
-      } else {
-          screen.textContent = screen.textContent + e.target.dataset.key;
-      }   
-  });
+	button.addEventListener('click', function(e) {
+			e.preventDefault();
+			
+			if (e.target.dataset.key == 'equal') {
+				screen.textContent = eval(screen.textContent);
+				
+				if (screen.textContent.length > 8) {
+					screen.textContent = eval(screen.textContent).toFixed(8);
+				}
+				} else if (e.target.dataset.key == 'reset') {
+					screen.textContent = '';
+				} else if (e.target.dataset.key == 'clear') {
+					let clear = screen.textContent.split('');
+					clear.pop()
+					screen.textContent = clear.join('');
+				} else {
+					screen.textContent = screen.textContent + e.target.dataset.key;
+				}   
+	});
 }
+
+
+
+document.addEventListener('keydown', (event) => {
+    var keyValue = event.key;
+    var codeValue = event.code;
+  
+    console.log(keyValue);
+    console.log(codeValue);
+  }, false);
